841796aeebbd621c745dafa4faa61861d136fc6a
John Victor Stephen Durairaj <jh312868@dal.ca>
2023-12-15 21:44:28 -0400

    implemented the 3d model and fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const  App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div clasName="relative z-0">
+        <div className="relative z-0">
           <Contact /> 
           <StarsCanvas />
         </div>
